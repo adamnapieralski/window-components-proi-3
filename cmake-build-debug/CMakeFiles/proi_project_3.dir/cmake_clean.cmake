3d62dab52c027cada6f63cb44573d8f6c3465da5
adamnapieralski <adam.napieralski@gmail.com>
2019-05-16 19:20:32 +0200

    Initial project files upload

---
+++ cmake-build-debug/CMakeFiles/proi_project_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proi_project_3.dir/main.cpp.o"
+  "proi_project_3.pdb"
+  "proi_project_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proi_project_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
